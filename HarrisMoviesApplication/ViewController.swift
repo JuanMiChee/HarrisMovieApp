a4a8b68f333950b9016663f5c8f1ec1aa1f29489
JuanHarrington <Harringtonjuanc@gmail.com>
2022-03-20 21:05:31 -0500

    creating new moves project with apis information

---
+++ HarrisMoviesApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HarrisMoviesApplication
+//
+//  Created by juan harrington on 20/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        //creating
+    }
+
+
+}
+
